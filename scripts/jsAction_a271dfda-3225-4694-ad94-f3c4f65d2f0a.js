07403cb481ae0ae98dbb55accec79e3604bf9d1a
Nicolas <nhubert@henix.fr>
2020-09-18 15:53:02 +0200

    depot

---
+++ scripts/jsAction_a271dfda-3225-4694-ad94-f3c4f65d2f0a.js
@@ -0,0 +1,9 @@
+﻿// Javascript skeleton. // Edit and adapt to your needs. // The documentation of the NeoLoad Javascript API // is available in the appendix of the documentation.
+
+// Get variable value from VariableManager 
+var choix = context.variableManager.getValue("animal"); var time = context.variableManager.getValue("time");
+
+if (choix==null) { context.fail("Variable 'item' not found"); }
+
+// Do some computation using the methods // you defined in the JS Library 
+logger.debug("The item is:   "+choix+"  The time is:   "+time+"  The LG is:   "+context.currentLG.name);
